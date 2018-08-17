bdc60d3b899d4a69e32b46c1514f91a9b85f2c42
JuvenBoy <lhcxx@outlook.com>
2018-08-18 00:06:59 +0800

    another day

--- src/main/java/com/leetcode/String/LeetcodeString.java
+++ src/main/java/com/leetcode/String/LeetcodeString.java
@@ -53,8 +53,25 @@ public class LeetcodeString {
     }
 
     public static boolean isPalindrome(String s) {
-        
+        if (s.length() <= 1)
+            return true;
+        char[] chars = s.toLowerCase().toCharArray();
+        for (int st = 0, ed = chars.length - 1; st <= ed; st++, ed--) {
+            while(st < ed && !isValidChar(s, st))
+                st++;
+            while (st < ed && !isValidChar(s, ed))
+                ed--;
+            if(chars[st] != chars[ed])
+                return false;
+        }
+        return true;
     }
+
+    private static boolean isValidChar(String s,int i) {
+        char c = s.charAt(i);
+        return (c >= '0' && c <= '9') ||(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+
 
 
 }
