89945896245531f8b75632a10ca35846c834f72c
huichun.lu <huichun.lu@intel.com>
2019-04-25 09:00:39 +0800

    adding tree

--- src/main/java/com/leetcode/tree/BinaryTreeTilt_Leetcode563.java
+++ src/main/java/com/leetcode/tree/BinaryTreeTilt_Leetcode563.java
@@ -1,8 +1,24 @@
 package com.leetcode.tree;
 
+import common.TreeNode;
+
 /**
  * Created by lhcxx on 19/4/23.
  */
 public class BinaryTreeTilt_Leetcode563 {
+	int res = 0;
 
+	public int findTilt(TreeNode root) {
+		helper(root);
+		return res;
+	}
+
+	private int helper(TreeNode root) {
+		if (root == null)
+			return 0;
+		int left = helper(root.left);
+		int right = helper(root.right);
+		res += Math.abs(left - right);
+		return left + right + root.value;
+	}
 }
---
+++ src/main/java/com/leetcode/tree/FindLeavesOfBinaryTree_Leetcode366.java
@@ -0,0 +1,25 @@
+package com.leetcode.tree;
+
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindLeavesOfBinaryTree_Leetcode366 {
+	public List<List<Integer>> findLeaves(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<>();
+
+	}
+
+	private int helper(List<List<Integer>>res, TreeNode root) {
+		if (root == null)
+			return -1;
+		int left = helper(res, root.left);
+		int right = helper(res, root.right);
+
+	}
+}
+
+
+
+ƒ
